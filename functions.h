6767ea65db07616430a7a60cbd903a21e706afce
Darren Ytsma <darren.ytsma@gmail.com>
2020-03-12 19:25:14 -0700

    Update and rename FUNctions.h to functions.h

--- functions.h
+++ functions.h
@@ -1,16 +1,8 @@
-/*
-Name:Darren Ytsma
-student number:301171125
-SFU username:dytsma
-Lecture section: CPMT 300 D100
-instructor: Brian Booth
-TA: Scott Kristjanson
-*/
 
 #include <time.h>
 #include <string.h>
-#ifndef _FUNctions_H_
-#define _FUNctions_H_
+#ifndef _functions_H_
+#define _functions_H_
 
 char* getTime();
 void inputs(int inputs);
